c39202977b4ea7474dae4768dcdcce33f984626c
Paul Croitoriu <contact@paulcroitoriu.com>
2019-01-31 19:54:56 +0000

    changed context

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi:"there" });
+  res.send("<h1 style=color:red;> Hello Paul</h1>");
 });
 
 const PORT = process.env.PORT || 5000;
