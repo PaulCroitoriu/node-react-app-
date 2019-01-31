5de355b5889232e66af725fb2d000bc38be02c5d
Paul Croitoriu <contact@paulcroitoriu.com>
2019-01-31 20:02:00 +0000

    changed route

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get('/', (req, res) => {
-  res.send("<h1 style=color:red;> Hello Paul</h1>");
+app.get('/home', (req, res) => {
+  res.send("<h1 style=color:gray;> Hello <a href='#'>Paul</a></h1>")
 });
 
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT)
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+//
+// app.get('/', (req, res) => {
+//   res.send("<h1 style=color:red;> Hello Paul</h1>");
+// });
+//
+// const PORT = process.env.PORT || 5000;
+//
+// app.listen(PORT)
